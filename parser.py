0a6a6c34553b9c912e23854831e89b925d53ac48
James Alexander <jalexa94@euclid.cs.nmt.edu>
2015-07-22 11:52:19 -0600

    creating actual linked lst

--- parser.py
+++ parser.py
@@ -5,10 +5,12 @@ def main():
     tsv = open("apps.tsv", 'r')
     apps = tsv.read()
     apps = apps.split('\n')
-    print(apps)
-    #head = student.Student(apps(1))
-    print (head.fname)    
-
+    tmp = apps[1]
+    head = student.Student(tmp)
+    lst = student.ListStudent(head)
+    for i in range (2, len(apps)):
+        #lst.insert(student.Student(apps[i]))
+     
     tsv.close()
 
 if __name__ == '__main__':
